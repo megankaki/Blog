c76964a5e9a891c608ca3844bb53a15cf242307c
Megan Kakitsubo <megankaki@gmail.com>
2015-06-02 18:19:42 -0700

    user model, comment model, post model, AND  users controller, comments controller, posts controller, sessions controller, bcrypt, authorization

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+  before_action :authorized?, except: :index
+
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	post = Post.new(post_params)
+    post.user = current_user
+
+  	if post.save
+  		redirect_to posts_path
+  	else 
+  		render :new
+  	end
+  end
+
+  private
+  def post_params
+  	params.require(:post).permit(:title, :body, :image)
+  end
+
+
+
+end
