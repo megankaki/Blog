16f51acb63caed75d00bc3afe3f707b7517a8a9e
Megan Kakitsubo <megankaki@gmail.com>
2015-06-04 21:39:51 -0700

    flash notice for login. started validating for posts.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,17 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:login][:password]) 
       session[:user_id] = user.id.to_s
+      flash[:notice] = "You have successfully logged in."
       redirect_to user_path(current_user)
     else
+      flash[:error] = "Failed to log in."
       render :new
     end
   end
 
   def destroy
     session.delete(:user_id)
+    "You have successfully logout."
     redirect_to login_path
   end
 end
